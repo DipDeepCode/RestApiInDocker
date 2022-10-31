42782e78404839b2ff18c56c316bc8b8a39d500e
Sergey Kovalev <dipdeepcode@mail.ru>
2022-10-31 22:54:19 +0300

    добавлены классы сущности, репозитория, изменен класс RestapiApplication

--- src/main/java/ru/sf/restapi/RestapiApplication.java
+++ src/main/java/ru/sf/restapi/RestapiApplication.java
@@ -1,13 +1,44 @@
 package ru.sf.restapi;
 
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import ru.sf.restapi.entity.Customer;
+import ru.sf.restapi.repository.CustomerRepository;
 
+import java.util.List;
+
+@Log
 @SpringBootApplication
 public class RestapiApplication {
+
+    private final CustomerRepository repository;
+
+    @Autowired
+    public RestapiApplication(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        List<Customer> allCustomers = this.repository.findAll();
+        log.info("Number of customers: " + allCustomers.size());
+
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstName("John");
+        newCustomer.setLastName("Doe");
+        log.info("Saving new customer...");
+        this.repository.save(newCustomer);
+
+        allCustomers = this.repository.findAll();
+        log.info("Number of customers: " + allCustomers.size());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestapiApplication.class, args);
     }
-
 }
---
+++ src/main/java/ru/sf/restapi/entity/Customer.java
@@ -0,0 +1,23 @@
+package ru.sf.restapi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+}
